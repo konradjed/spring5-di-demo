ce404236496539e8d0e82dac1c0e9b7b74d83ddb
Konrad Jedrzejewski <kjedrzejewski@bluesoft.net.pl>
2018-07-25 09:18:27 +0200

    DI with spring

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework;
 
+import guru.springframework.controllers.ConstructorInjectorController;
 import guru.springframework.controllers.MyController;
+import guru.springframework.controllers.PropertyInjectedController;
+import guru.springframework.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,5 +18,9 @@ public class DiDemoApplication {
         MyController controller = (MyController) ctx.getBean("myController");
 
         controller.hello();
+
+        System.out.println(ctx.getBean(ConstructorInjectorController.class).sayHello());
+        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
     }
 }
--- src/main/java/guru/springframework/controllers/ConstructorInjectorController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectorController.java
@@ -1,11 +1,15 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectorController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public ConstructorInjectorController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
